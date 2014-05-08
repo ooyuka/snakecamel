809b3fec9a43725641b6e04494303d5bec027b41
g12942oy <g12942oy+git@gm.tsuda.ac.jp>
2014-05-08 16:42:08 +0900

    エラー修正

--- snake-camel/test/snakecamel/SnakeCamelTest.java
+++ snake-camel/test/snakecamel/SnakeCamelTest.java
@@ -1,7 +1,7 @@
 package snakecamel;
 
 import static org.junit.Assert.*;
-
+import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
 
 public class SnakeCamelTest {
@@ -12,7 +12,7 @@ public class SnakeCamelTest {
 		String s = "Sss";
 		String t = scu.capitalize("sss");
 		System.out.println(t);
-		assertEquals(s,t);
+		assertThat(s,is(t));
 	}	
 	
 	@Test
@@ -21,7 +21,7 @@ public class SnakeCamelTest {
 		String s = " ";
 		String t = scu.capitalize(" ");
 		System.out.println(t);
-		assertEquals(s,t);
+		assertThat(s,is(t));
 	}	
 	
 	@Test
@@ -30,7 +30,7 @@ public class SnakeCamelTest {
 		String s = "A";
 		String t = scu.capitalize("a");
 		System.out.println(t);
-		assertEquals(s,t);
+		assertThat(s,is(t));
 	}	
 	
 	@Test
@@ -39,7 +39,7 @@ public class SnakeCamelTest {
 		String s = "Xyz";
 		String t = scu.capitalize("xyz");
 		System.out.println(t);
-		assertEquals(s,t);
+		assertThat(s,is(t));
 	}	
 	
 	@Test
@@ -48,7 +48,7 @@ public class SnakeCamelTest {
 		String s = "tTT";
 		String t = scu.uncapitalize("TTT");
 		System.out.println(t);
-		assertEquals(s,t);
+		assertThat(s,is(t));
 	}	
 	
 	@Test
@@ -57,7 +57,7 @@ public class SnakeCamelTest {
 		String s = " ";
 		String t = scu.uncapitalize(" ");
 		System.out.println(t);
-		assertEquals(s,t);
+		assertThat(s,is(t));
 	}	
 	
 	@Test
@@ -66,25 +66,25 @@ public class SnakeCamelTest {
 		String s = "xyz";
 		String t = scu.uncapitalize("Xyz");
 		System.out.println(t);
-		assertEquals(s,t);
+		assertThat(s,is(t));
 	}	
 	
 	@Test
 	public void snakeToCamelcaseを使う() {
 		SnakeCamelUtil scu = new SnakeCamelUtil();
-		String s = "Snake";
+		String s = "SnakeCase";
 		String t = scu.snakeToCamelcase("snake_case");
 		System.out.println(t);
-		assertEquals(s,t);
+		assertThat(s,is(t));
 	}	
 	
 	@Test
 	public void snakeToCamelcaseを使う2() {
 		SnakeCamelUtil scu = new SnakeCamelUtil();
-		String s = "MyNameIs";
+		String s = "MyNameIsOoyuka";
 		String t = scu.snakeToCamelcase("my_name_is_ooyuka");
 		System.out.println(t);
-		assertEquals(s,t);
+		assertThat(s,is(t));
 	}
 	
 	@Test
@@ -93,7 +93,7 @@ public class SnakeCamelTest {
 		String s = "Abc";
 		String t = scu.snakeToCamelcase("abc");
 		System.out.println(t);
-		assertEquals(s,t);
+		assertThat(s,is(t));
 	}	
 	
 	@Test
@@ -102,7 +102,7 @@ public class SnakeCamelTest {
 		String s = "AbcDef";
 		String t = scu.snakeToCamelcase("abc_def");
 		System.out.println(t);
-		assertEquals(s,t);
+		assertThat(s,is(t));
 	}	
 	
 	@Test
@@ -111,7 +111,7 @@ public class SnakeCamelTest {
 		String s = "AbcDefGh";
 		String t = scu.snakeToCamelcase("abc_def_gh");
 		System.out.println(t);
-		assertEquals(s,t);
+		assertThat(s,is(t));
 	}	
 	
 	@Test
@@ -120,7 +120,7 @@ public class SnakeCamelTest {
 		String s = "AbcDefGh";
 		String t = scu.snakeToCamelcase("abc_def___gh");
 		System.out.println(t);
-		assertEquals(s,t);
+		assertThat(s,is(t));
 	}	
 	
 	@Test
@@ -129,25 +129,25 @@ public class SnakeCamelTest {
 		String s = "AbcDef";
 		String t = scu.snakeToCamelcase("_abc_def__");
 		System.out.println(t);
-		assertEquals(s,t);
+		assertThat(s,is(t));
 	}	
 	
 	@Test
 	public void camelToSnakecaseを使う() {
 		SnakeCamelUtil scu = new SnakeCamelUtil();
-		String s = "cCamel_cCase";
+		String s = "camel_case";
 		String t = scu.camelToSnakecase("CamelCase");
 		System.out.println(t);
-		assertEquals(s,t);
+		assertThat(s,is(t));
 	}	
 	
 	@Test
 	public void camelToSnakecaseを使う2() {
 		SnakeCamelUtil scu = new SnakeCamelUtil();
-		String s = "my_nName_iIs_oOoyuka";
+		String s = "my_name_is_ooyuka";
 		String t = scu.camelToSnakecase("myNameIsOoyuka");
 		System.out.println(t);
-		assertEquals(s,t);
+		assertThat(s,is(t));
 	}	
 	
 	@Test
@@ -156,24 +156,24 @@ public class SnakeCamelTest {
 		String s = "abc";
 		String t = scu.camelToSnakecase("Abc");
 		System.out.println(t);
-		assertEquals(s,t);
+		assertThat(s,is(t));
 	}
 	
 	@Test
 	public void camelToSnakecaseを使う4() {
 		SnakeCamelUtil scu = new SnakeCamelUtil();
-		String s = "aAbc_dDef";
+		String s = "abc_def";
 		String t = scu.camelToSnakecase("AbcDef");
 		System.out.println(t);
-		assertEquals(s,t);
+		assertThat(s,is(t));
 	}
 	
 	@Test
 	public void camelToSnakecaseを使う5() {
 		SnakeCamelUtil scu = new SnakeCamelUtil();
-		String s = "aAbc_dDef_gGh";
+		String s = "abc_def_gh";
 		String t = scu.camelToSnakecase("AbcDefGh");
 		System.out.println(t);
-		assertEquals(s,t);
+		assertThat(s,is(t));
 	}
 }
